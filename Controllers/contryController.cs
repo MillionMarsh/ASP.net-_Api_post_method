364c695b8d6cf8e74ee4312902cc5bb6160adf17
marsh <marsh@MILLION_MARSH>
2023-03-28 00:21:25 +0530

    Add project files.

---
+++ Controllers/contryController.cs
@@ -0,0 +1,25 @@
+﻿using apipractice.Data;
+using apipractice.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apipractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class contryController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public contryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public ActionResult<Country> create([FromBodyAttribute]Country country)
+            {
+                _context.countries.Add(country);
+                _context.SaveChanges();
+                return Ok();
+            }
+    }
+}
---
+++ Models/Country.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apipractice.Models
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(5)]
+        public string shortname { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string countrycode { get; set; }
+    }
+}
